521b74163bdd516eab39d529a0bbfaabf044a1e2
amehmichaeldev <141594454+amehmichaeldev@users.noreply.github.com>
2024-09-04 01:34:48 +0100

    Add files via upload

---
+++ js/words.js
@@ -0,0 +1,166 @@
+let wordList = [
+    {
+        word: "python",
+        hint: "Programming Language"
+    },
+    {
+        word: "guitar",
+        hint: "A musical instrument"
+    },
+    {
+        word: "aim",
+        hint: "A purpose or intention"
+    },
+    {
+        word: "venus",
+        hint: "Planet of our solar system"
+    },
+    {
+        word: "gold",
+        hint: "A yellow precious metal"
+    },
+    {
+        word: "ebay",
+        hint: "Online shopping site"
+    },
+    {
+        word: "java",
+        hint: "Programming Language"
+    },
+    {
+        word: "coding",
+        hint: "Related to programming"
+    },
+    {
+        word: "matrix",
+        hint: "Science fiction movie"
+    },
+    {
+        word: "bugs",
+        hint: "Related to programming"
+    },
+    {
+        word: "avatar",
+        hint: "Epic science fiction film"
+    },
+    {
+        word: "gif",
+        hint: "A file format for image"
+    },
+    {
+        word: "mental",
+        hint: "Related to the mind"
+    },
+    {
+        word: "map",
+        hint: "Diagram represent of an area"
+    },
+    {
+        word: "island",
+        hint: "Land surrounded by water"
+    },
+    {
+        word: "hockey",
+        hint: "A famous outdoor game"
+    },
+    {
+        word: "chess",
+        hint: "Related to a board game"
+    },
+    {
+        word: "facebook",
+        hint: "A social media app"
+    },
+    {
+        word: "github",
+        hint: "Code hosting platform"
+    },
+    {
+        word: "png",
+        hint: "A image file format"
+    },
+    {
+        word: "silver",
+        hint: "Precious greyish-white metal"
+    },
+    {
+        word: "mobile",
+        hint: "An electronic device"
+    },
+    {
+        word: "gpu",
+        hint: "Computer component"
+    },
+    {
+        word: "google",
+        hint: "Famous search engine"
+    },
+    {
+        word: "venice",
+        hint: "Famous city of waters"
+    },
+    {
+        word: "excel",
+        hint: "Microsoft product for windows"
+    },
+    {
+        word: "flute",
+        hint: "A musical instrument"
+    },
+    {
+        word: "crypto",
+        hint: "Related to cryptocurrency"
+    },
+    {
+        word: "tesla",
+        hint: "Unit of magnetic flux density"
+    },
+    {
+        word: "mars",
+        hint: "Planet of our solar system"
+    },
+    {
+        word: "email",
+        hint: "Related to exchanging message"
+    },
+    {
+        word: "air",
+        hint: "Related to a gas"
+    },
+    {
+        word: "idea",
+        hint: "A thought or suggestion"
+    },
+    {
+        word: "svg",
+        hint: "A vector image format"
+    },
+    {
+        word: "jpeg",
+        hint: "A image file format"
+    },
+    {
+        word: "search",
+        hint: "Act to find something"
+    },
+    {
+        word: "key",
+        hint: "Small piece of metal"
+    },
+    {
+        word: "egypt",
+        hint: "A country name"
+    },
+    {
+        word: "joker",
+        hint: "Psychological thriller film"
+    },
+    {
+        word: "photo",
+        hint: "Representation of person or scene"
+    },
+    {
+        word: "rain",
+        hint: "Related to a water"
+    },
+]
